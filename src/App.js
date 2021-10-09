dc5e2eadddeffcaa5f87ef12f3c540f53e62e02e
dnnzz <l1611012074@stud.sdu.edu.tr>
2021-10-09 19:25:26 +0300

    backend done & layout done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./components/Header";
+import Options from "./components/Options";
+import Chart from "./components/Chart";
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <Options />
+      <Chart />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Options.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Options() {
+  return (
+    <div className='options-container'>
+      <div className='javascript'>
+        <h2>Javascript</h2>
+      </div>
+      <div className='go'>
+        <h2>Go</h2>
+      </div>
+      <div className='ruby'>
+        <h2>Ruby</h2>
+      </div>
+      <div className='cplus'>
+        <h2>C++</h2>
+      </div>
+    </div>
+  );
+}
